992c38dd7c53734ef12b57680460256c572d12fa
Brunoiooo <>
2025-10-09 23:58:56 +0200

    Dx

--- src/Emulator.py
+++ src/Emulator.py
@@ -871,7 +871,6 @@ class Emulator:
         reward += self.rewardMap()
         reward += self.panishWorldIllegalMoves(primary_memo_before, reward)
         reward += self.panishMenuIllegalMoves(primary_memo_before)
-        reward += self.panishSwitchMenu()
         reward += self.panishMenuSelect(primary_memo_before)
         reward += self.panishMenuPosition(primary_memo_before)
         reward += self.panishMenuIn(reward)
