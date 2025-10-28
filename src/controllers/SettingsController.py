65207b35638cc998f67df5db7746e6c18dffd166
Błażej <>
2025-10-28 19:51:40 +0100

    add

--- src/controllers/SettingsController.py
+++ src/controllers/SettingsController.py
@@ -6,87 +6,88 @@ from views.SettingsView import SettingsView
 
 class SettingsController:
     def __init__(self, view: SettingsView, model: SettingsModel):
-        self.view = view
-        self.model = model
+        self.__view = view
+        self.__model = model
 
-        self.view.string_var_create_profile.trace_add(
+        self.__view.string_var_create_profile.trace_add(
             "write", self.handle_string_var_create_profile
         )
-        self.view.button_create_profile.configure(
+        self.__view.button_create_profile.configure(
             command=self.handle_button_create_profile
         )
-        self.view.listbox_profiles.bind(
+        self.__view.listbox_profiles.bind(
             "<<ListboxSelect>>", self.handle_listbox_profiles
         )
-        self.view.button_delete_profile.configure(
+        self.__view.button_delete_profile.configure(
             command=self.handle_button_delete_profile
         )
-        self.view.boolean_var_settings_debug.trace_add(
+        self.__view.boolean_var_settings_debug.trace_add(
             "write", self.handle_boolean_var_settings_debug
         )
 
-        self.refresh()
+        self.__refresh()
 
-    def refresh(self):
-        self.view.load_profiles(self.model.profiles)
-        self.view.button_create_profile.configure(
+    def __refresh(self):
+        self.__view.load_profiles(self.__model.profiles)
+        self.__view.button_create_profile.configure(
             state=(
                 "normal"
-                if len(self.view.string_var_create_profile.get())
-                and self.view.string_var_create_profile.get() not in self.model.profiles
+                if len(self.__view.string_var_create_profile.get())
+                and self.__view.string_var_create_profile.get()
+                not in self.__model.profiles
                 else "disabled"
             )
         )
-        self.view.button_delete_profile.configure(
-            state="normal" if self.view.selected_value() else "disabled"
+        self.__view.button_delete_profile.configure(
+            state="normal" if self.__view.selected_value() else "disabled"
         )
-        if self.model.settings is not None:
-            self.view.load_settings(self.model.settings)
-            self.view.frame_settings.grid(row=0, column=1, sticky="nsew")
+        if self.__model.settings is not None:
+            self.__view.load_settings(self.__model.settings)
+            self.__view.frame_settings.grid(row=0, column=1, sticky="nsew")
         else:
-            self.view.frame_settings.grid_forget()
+            self.__view.frame_settings.grid_forget()
 
     def handle_string_var_create_profile(self, *args):
-        self.refresh()
+        self.__refresh()
 
     def handle_button_create_profile(self):
         try:
-            self.model.create_profile(self.view.string_var_create_profile.get())
+            self.__model.create_profile(self.__view.string_var_create_profile.get())
         except Exception as e:
             messagebox.showerror("handle_button_create_profile", e)
         finally:
-            self.refresh()
+            self.__refresh()
 
     def handle_listbox_profiles(self, *args):
         try:
-            self.model.profile = self.view.selected_value()
+            self.__model.profile = self.__view.selected_value()
         except Exception as e:
             messagebox.showerror("handle_listbox_profiles", e)
         finally:
-            self.refresh()
+            self.__refresh()
 
     def handle_button_delete_profile(self):
         try:
-            profile = self.view.selected_value()
+            profile = self.__view.selected_value()
 
             if not profile:
                 raise ValueError("Profile has not selected.")
 
-            self.model.delete_profile(profile)
+            self.__model.delete_profile(profile)
         except Exception as e:
             messagebox.showerror("handle_button_delete_profile", e)
         finally:
-            self.refresh()
+            self.__refresh()
 
     def handle_boolean_var_settings_debug(self, *args):
         try:
-            if self.model.settings is None:
+            if self.__model.settings is None:
                 raise ValueError("Profile has not selected.")
 
-            settings = self.model.settings
-            settings["is_debug"] = self.view.boolean_var_settings_debug.get()
-            self.model.settings = settings
+            settings = self.__model.settings
+            settings["is_debug"] = self.__view.boolean_var_settings_debug.get()
+            self.__model.settings = settings
         except Exception as e:
             messagebox.showerror("handle_boolean_var_settings_debug", e)
         finally:
-            self.refresh()
+            self.__refresh()
