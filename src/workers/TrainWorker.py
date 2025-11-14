899e26539a914240628fc45cbdb47e9f1afaa2d4
Brunoiooo <>
2025-11-14 20:04:24 +0100

    up to 20

--- src/workers/TrainWorker.py
+++ src/workers/TrainWorker.py
@@ -43,7 +43,7 @@ class TrainWorker:
     epsilon_end = 0.05
     epsilon_decay_count = 2000000
     ckpt_every = 10000
-    evaluate_greedy_times = 1
+    evaluate_greedy_times = 20
 
     __event_stop: None | Event = None
 
